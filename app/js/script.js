f98d05a057e0f9d3cc2e814e7ac25865e63a4da6
Plintrapire <andrey.ovchinnicov3.0@mail.ru>
2018-12-07 17:49:35 +0300

    PSD_2

---
+++ app/js/script.js
@@ -0,0 +1,58 @@
+
+// $(document).ready(function(){
+
+//  $(".menu").click(function(){ 
+//     $(".podpunkt").slideToggle(1000);
+//   });
+
+//   $(".reg").click(function(){ 
+//     $(".register").slideToggle(1000);
+//   });
+
+//   $(".sign").click(function(){ 
+//     $(".sign-obc").slideToggle(1000);
+//   });
+
+//   $(".car1-obc").click(function(){
+//    $(".car1-obc-xar").slideToggle(1000);
+//   $(".car1-obc-info").css("height","717");
+//   }); 
+
+
+//   $(".car2-obc").click(function(){ 
+//    $(".car2-obc-xar").slideToggle(1000);
+//   $(".car2-obc-info").css("height","717");
+//   });
+// });
+
+$(document).ready(function(){
+
+      $(".span1").click(function(event){ 
+          $('html').animate({scrollTop: 880}, 1500);
+      });
+
+      $(".span2").click(function(event){ 
+          $('html').animate({scrollTop: 880+740}, 1500);
+
+      });
+
+      $(".span3").click(function(event){ 
+          $('html').animate({scrollTop: 880+740+925}, 1500);
+
+      });
+
+      $(".span4").click(function(event){ 
+          $('html').animate({scrollTop: 880+740+925+818+202}, 1500);
+
+      });
+
+      $(".span5").click(function(event){ 
+          $('html').animate({scrollTop: 880+740+925+818+356+202}, 1500);
+
+      });
+
+      $(".span6").click(function(event){ 
+          $('html').animate({scrollTop: 880+740+925+818+356+850+202}, 1500);
+
+      });
+  });
