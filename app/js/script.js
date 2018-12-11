209f6a625e3315a5f35143686982e481320c1fff
Plintrapire <andrey.ovchinnicov3.0@mail.ru>
2018-12-11 17:09:00 +0300

    form

--- app/js/script.js
+++ app/js/script.js
@@ -27,6 +27,9 @@
 
 $(document).ready(function(){
 
+
+
+
       $(".span1").click(function(event){ 
           $('html').animate({scrollTop: 880}, 1500);
       });
@@ -61,27 +64,128 @@ $(document).ready(function(){
 $('.scroll').click(function(){ 
     if($(window).scrollTop()<880){ 
       $('html').animate({scrollTop: 880}, 1500);
+      $(".scroll").css("border","1px solid white");
+      $(".scroll2").css("border","1px solid white");
+      $(".scroll-obc p").css("color","white");
+      $(".scroll").css("backgroundImage", "url(../img/scroll.png)");
+      $(".scroll2").css("backgroundImage", "url(../img/scroll2.png)");
     }
     else { 
       if($(window).scrollTop()<1620){ 
         $('html').animate({scrollTop: 1620}, 1500);
+        $(".scroll").css("border","1px solid white");
+        $(".scroll2").css("border","1px solid white");
+        $(".scroll-obc p").css("color","white");
+        $(".scroll").css("backgroundImage", "url(../img/scroll.png)");
+        $(".scroll2").css("backgroundImage", "url(../img/scroll2.png)");
       }
       else {
         if($(window).scrollTop()< 2545){ 
           $('html').animate({scrollTop: 2545}, 1500);
           $(".scroll").css("border","1px solid black");
+          $(".scroll2").css("border","1px solid black");
+          $(".scroll-obc p").css("color","black");
+          $(".scroll").css("backgroundImage", "url(../img/scrollb.png)");
+          $(".scroll2").css("backgroundImage", "url(../img/scroll2b.png)");
         }
         else {
           if($(window).scrollTop()< 3565){ 
-          $('html').animate({scrollTop:  3565}, 1500);
+            $('html').animate({scrollTop:  3565}, 1500);
+            $(".scroll").css("border","1px solid white");
+            $(".scroll2").css("border","1px solid white");
+            $(".scroll-obc p").css("color","white");
+            $(".scroll").css("backgroundImage", "url(../img/scroll.png)");
+            $(".scroll2").css("backgroundImage", "url(../img/scroll2.png)");
           }
           else{
             if($(window).scrollTop()< 3921){ 
               $('html').animate({scrollTop:  3921}, 1500);
+              $(".scroll").css("border","1px solid white");
+              $(".scroll2").css("border","1px solid white");
+              $(".scroll-obc p").css("color","white");
+              $(".scroll").css("backgroundImage", "url(../img/scroll.png)");
+              $(".scroll2").css("backgroundImage", "url(../img/scroll2.png)");
             }
             else{
               if($(window).scrollTop()< 4771){ 
-               $('html').animate({scrollTop:  4771}, 1500);
+                $('html').animate({scrollTop:  4771}, 1500);
+                $(".scroll").css("border","1px solid black");
+                $(".scroll2").css("border","1px solid black");
+                $(".scroll-obc p").css("color","black");
+                $(".scroll").css("backgroundImage", "url(../img/scrollb.png)");
+                $(".scroll2").css("backgroundImage", "url(../img/scroll2b.png)");
+              }
+            }
+          }
+        } 
+      }
+    } 
+}); 
+// 
+
+$('.scroll2').click(function(){ 
+    if($(window).scrollTop()>4771){ 
+      $('html').animate({scrollTop: 4771}, 1500);
+      $(".scroll").css("border","1px solid white");
+      $(".scroll2").css("border","1px solid white");
+      $(".scroll-obc p").css("color","white");
+      $(".scroll").css("backgroundImage", "url(../img/scroll.png)");
+      $(".scroll2").css("backgroundImage", "url(../img/scroll2.png)");
+    }
+    else { 
+      if($(window).scrollTop()>3921){ 
+        $('html').animate({scrollTop: 3921}, 1500);
+        $(".scroll").css("border","1px solid white");
+        $(".scroll2").css("border","1px solid white");
+        $(".scroll-obc p").css("color","white");
+        $(".scroll").css("backgroundImage", "url(../img/scroll.png)");
+        $(".scroll2").css("backgroundImage", "url(../img/scroll2.png)");
+      }
+      else {
+        if($(window).scrollTop()>3565){ 
+          $('html').animate({scrollTop: 3565}, 1500);
+          $(".scroll").css("border","1px solid white");
+          $(".scroll2").css("border","1px solid white");
+          $(".scroll-obc p").css("color","white");
+          $(".scroll").css("backgroundImage", "url(../img/scroll.png)");
+          $(".scroll2").css("backgroundImage", "url(../img/scroll2.png)");
+        }
+        else {
+          if($(window).scrollTop()>2545){ 
+          $('html').animate({scrollTop: 2545}, 1500);
+          $(".scroll").css("border","1px solid black");
+          $(".scroll2").css("border","1px solid black");
+          $(".scroll-obc p").css("color","black");
+          $(".scroll").css("backgroundImage", "url(../img/scrollb.png)");
+          $(".scroll2").css("backgroundImage", "url(../img/scroll2b.png)");
+          }
+          else{
+            if($(window).scrollTop()>1620){ 
+              $('html').animate({scrollTop: 1620}, 1500);
+              $(".scroll").css("border","1px solid white");
+              $(".scroll2").css("border","1px solid white");
+              $(".scroll-obc p").css("color","white");
+              $(".scroll").css("backgroundImage", "url(../img/scroll.png)");
+              $(".scroll2").css("backgroundImage", "url(../img/scroll2.png)");
+            }
+            else{
+              if($(window).scrollTop()>880){ 
+                $('html').animate({scrollTop: 880}, 1500);
+                $(".scroll").css("border","1px solid white");
+                $(".scroll2").css("border","1px solid white");
+                $(".scroll-obc p").css("color","white");
+                $(".scroll").css("backgroundImage", "url(../img/scroll.png)");
+                $(".scroll2").css("backgroundImage", "url(../img/scroll2.png)");
+              }
+              else{
+              if($(window).scrollTop()>0){ 
+                $('html').animate({scrollTop: 0}, 1500);
+                $(".scroll").css("border","1px solid white");
+                $(".scroll2").css("border","1px solid white");
+                $(".scroll-obc p").css("color","white");
+                $(".scroll").css("backgroundImage", "url(../img/scroll.png)");
+                $(".scroll2").css("backgroundImage", "url(../img/scroll2.png)");
+                }
               }
             }
           }
@@ -90,10 +194,21 @@ $('.scroll').click(function(){
     } 
 }); 
 
-
-
-
+  $(function() {
+    $(window).scroll(function() {
+      if($(this).scrollTop() !=0) {
+        $('.scroll-obc').fadeIn();
+      }
+      else {
+        $('.scroll-obc').fadeOut();
+      }
+    });
   });
+
+
+
+
+});
 
 
       
